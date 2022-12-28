0624a657eb4e27e71c94b321f4dda246f723ef78
Gary Emmerton <gemmerton@hotmail.com>
2022-12-28 12:28:07 +0000

    wasconfig.ts template

--- PiBot-website/src/app/awsconfig - Copy.ts
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-export var region:string = "aws-region";
-export var iotEndpoint:string = "iot-endpoint";
-export var cognitoIdPoolId:string = "pibot-id-pool";
-export var cognitoUserPool:string = "pibot-user-pool";
-export var cognitoClientId:string = "pibot-app-id";
-export var cognitoIoTPolicy:string = "Pibot-web-policy";
