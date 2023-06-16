55524229ec39acc57c09230a51d1453f11e57deb
Gary Emmerton <gemmerton@hotmail.com>
2023-06-16 16:23:25 +0100

    dependency updates for provisioning app

--- PiBot-provisioning-app/src/app/authguard.service.ts
+++ PiBot-provisioning-app/src/app/authguard.service.ts
@@ -17,12 +17,12 @@
  */
 
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable()
 
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
 
   constructor(public auth: AuthService, public router: Router) {}
 
