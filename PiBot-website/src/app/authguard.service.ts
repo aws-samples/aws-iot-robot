3d58afce37e69192eaef1e218f852ec67f0b8453
Gary Emmerton <gemmerton@hotmail.com>
2023-06-18 14:17:21 +0100

    updated angular and dependencies in main web app

--- PiBot-website/src/app/authguard.service.ts
+++ PiBot-website/src/app/authguard.service.ts
@@ -1,11 +1,11 @@
 // src/app/auth/auth-guard.service.ts
 import { Injectable } from '@angular/core';
-import { Router, CanActivate } from '@angular/router';
+import { Router } from '@angular/router';
 import { AuthService } from './auth.service';
 
 @Injectable()
 
-export class AuthGuardService implements CanActivate {
+export class AuthGuardService  {
 
   constructor(public auth: AuthService, public router: Router) {}
 
